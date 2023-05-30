254efb4e96e2ab2b15912523b886d10ca905c4be
Paulo <paulosouzap7@gmail.com>
2023-05-29 23:26:48 -0300

    inclusão service e enviroments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/components/header/header.component';
+import { SearchComponente } from 'src/components/search/search.component'
 import { MainComponent } from 'src/components/main/main.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SearchComponente,
     MainComponent
   ],
   imports: [
